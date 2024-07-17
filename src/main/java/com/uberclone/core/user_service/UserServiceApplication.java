4898e90d5d9912c32af3fbe8a40136d0e3e9f6cc
ruwan liyanage <ruwanliyanagegalle1994@gmail.com>
2024-07-17 23:10:17 +0530

    Update UserServiceApplication.java

--- src/main/java/com/uberclone/core/user_service/UserServiceApplication.java
+++ src/main/java/com/uberclone/core/user_service/UserServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		//need to add the hibernate erro skip code
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
