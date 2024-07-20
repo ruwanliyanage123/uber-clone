2c158b707ff64c55b24faeac40f6f8e53665143b
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-07-20 11:11:00 +0530

    Attach the service layer

--- src/main/java/com/uberclone/core/userservice/controller/UserController.java
+++ src/main/java/com/uberclone/core/userservice/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.uberclone.core.userservice.controller;
 
 import com.uberclone.core.userservice.dto.UserDTO;
+import com.uberclone.core.userservice.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user-service")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @PostMapping()
     public ResponseEntity<UserDTO> creteUser(@RequestBody UserDTO userDTO) {
-        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+        final UserDTO dto = userService.createPatient(userDTO);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 }
