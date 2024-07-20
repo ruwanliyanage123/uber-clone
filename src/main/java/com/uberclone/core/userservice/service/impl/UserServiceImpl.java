ce9a19c394d80030a939a78254716a3632ea4c22
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-07-20 16:31:21 +0530

    Resolve commits and Add missed files

--- src/main/java/com/uberclone/core/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/uberclone/core/userservice/service/impl/UserServiceImpl.java
@@ -2,12 +2,14 @@ package com.uberclone.core.userservice.service.impl;
 
 import com.uberclone.core.userservice.dto.UserDTO;
 import com.uberclone.core.userservice.service.api.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public UserDTO createPatient(UserDTO userDTO) {
-        final UserDTO userDTO1 = new UserDTO();
-        userDTO1.setUserName(userDTO1.getUserName()+" appended");
-        return userDTO1;
+        userDTO.setUserName(userDTO.getUserName()+" appended by service");
+        userDTO.setFirstName(userDTO.getFirstName()+" appended by service");
+        return userDTO;
     }
 }
